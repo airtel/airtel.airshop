fd0966549cec503542dbe8f747ca63cfda1dd114
airtel <info@airtel.lv>
2012-12-01 21:06:13 +0200

    Code optimization and major bugfixes

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://test.airtel.lv/newshop/';
+$config['base_url']	= 'http://systest.airtel.lv/airshop/';
 
 /*
 |--------------------------------------------------------------------------
